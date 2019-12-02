dcb16929d9b77623dcae52ba251013b50b108088
Kirithiv R <52394696+kirithiv2000@users.noreply.github.com>
2019-12-02 16:47:52 +0530

    Update zomato_acess_key.py

--- zomato_acess_key.py
+++ zomato_acess_key.py
@@ -6,14 +6,28 @@ from find_geolocation import find_geolocation
 latlan=find_geolocation()
 latlan=latlan.split(" ")
 lat,lon="lat="+latlan[0][:-1:],"lon="+latlan[2][:-1:]
-geocode="geocode?"+lat+"%20%20&"+lon
+geocode="geocode?"+lat+"&"+lon
 requestsurl="https://developers.zomato.com/api/v2.1/"
 categories="categories"
 geocodeurl=requestsurl+geocode
 print(geocodeurl)
 r = requests.get(geocodeurl, headers={"user_key": "a79201374373ff188cca7a39151e5cd8", "Accept": "application/json"})
 r=json.loads(r.text)
-pprint.pprint(r)
+# pprint.pprint(r)
+for restaurant in r["nearby_restaurants"]:
+	print("Shop Name                : ",restaurant["restaurant"]["name"])
+	print("Average cost of 2 person : ",restaurant["restaurant"]["average_cost_for_two"])
+	print("Accepted Currency        : ",restaurant["restaurant"]["currency"])
+	print("Available food           : ",restaurant["restaurant"]["cuisines"])
+	print("Rating                   : ",restaurant["restaurant"]["user_rating"]["aggregate_rating"])
+	print("Votes                    : ",restaurant["restaurant"]["user_rating"]["votes"])
+	print("Menu url                 : ",restaurant["restaurant"]["menu_url"])
+	print("Url                      : ",restaurant["restaurant"]["url"])
+	print("Address                  : ",restaurant["restaurant"]["location"]["address"])
+	for rating in restaurant["restaurant"]["user_rating"]:
+		if rating=="rating_tool_tip":
+			print("Reason of Rating         : ",restaurant["restaurant"]["user_rating"]["rating_tool_tip"])
+	print("--------------------------------------------------------------------------------------")
 
 
 
